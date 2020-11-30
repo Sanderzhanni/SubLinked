72bb8d363fe391c150f585c9b792b2c9a2c9fd69
Sanderzhanni <hannis@tlu.ee>
2020-11-30 08:31:35 +0200

    build and server serving changes

--- server/server.js
+++ server/server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const apiRouter = require("./apiRouter.js");
+const path = require("path");
 require("dotenv").config();
 
 app.use(apiRouter);
 
-/** For images and bundle.js */
-app.use("/static", express.static("build/static"));
+// add middlewares
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
 
-/** For index.html */
-app.use("/*", express.static("build"));
+// For index.html
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
