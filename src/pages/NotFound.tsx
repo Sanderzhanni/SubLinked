6f0fe6348869ae69f094bf9e3a78d8bc02a4079d
Sanderzhanni <hannis@tlu.ee>
2020-12-01 03:18:38 +0200

    not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import SentimentVeryDissatisfiedOutlinedIcon from '@material-ui/icons/SentimentVeryDissatisfiedOutlined';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 const NotFound = (): React.ReactElement => (
   <>
-    <div>Can no find this page</div>
+    <Box>
+      <Box display="flex" justifyContent="center" m={1} p={1}>
+        <SentimentVeryDissatisfiedOutlinedIcon style={{ fontSize: 175 }} />
+      </Box>
+      <Box display="flex" justifyContent="center" m={1} p={1}>
+        <Typography component="h1" variant="h2">
+          Error 404
+        </Typography>
+      </Box>
+      <Box display="flex" justifyContent="center" m={1} p={1}>
+        <Typography component="h2" variant="h5" align="center">
+          Whoops. Looks like this page does not exist.
+        </Typography>
+      </Box>
+    </Box>
   </>
 );
 
